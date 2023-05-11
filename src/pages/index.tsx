9bbf7da8f3b1ba087e9d255491557087b502504a
IestynGage <ixg@corefiling.com>
2023-05-11 16:37:21 +0100

    Tidy up root index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,14 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <Layout>
-      Welcome to the official blog of David Envull, Communications Director at CoreFiling. 
-      With over a decade of experience in the communications industry, 
-      David is a seasoned expert in crafting effective messaging strategies that engage and inform audiences.
+      <Head>
+        <title> David Envull blog </title>
+      </Head>
+      <p>
+        Welcome to the official blog of David Envull, Communications Director at CoreFiling. 
+        With over a decade of experience in the communications industry, 
+        David is a seasoned expert in crafting effective messaging strategies that engage and inform audiences.
+      </p>
     </Layout>
   )
 }
