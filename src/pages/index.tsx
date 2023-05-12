8383138f3d8f1b4171cbf227149d4e85a9b0351a
Iestyn <>
2023-05-12 23:26:51 +0100

    Add image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import Layout from "@/components/layout";
 import utilStyles from "../styles/Utils.module.css";
+import indexStyles from "../styles/RootIndex.module.css";
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +14,14 @@ export default function Home() {
         <title> David Envull blog </title>
       </Head>
       <div className={`${utilStyles.centerItems}`}>
+        <Image
+          style={{marginBottom:'5em', marginTop:'5em'}}
+          src="/images/digital-portrait.png"
+          className={indexStyles.centerItems}
+          height={300}
+          width={300}
+          alt={""}
+          />
         <p className={`${utilStyles.displayContent}`}>
           Welcome to the official blog of David Envull, Communications Director at
           CoreFiling. With over a decade of experience in the communications
